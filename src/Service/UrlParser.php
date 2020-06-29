53f0080c3c0bae76f8923e414bafd2a9384f1180
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-06-29 15:16:51 +0200

    Fix some edge cases

--- src/Service/UrlParser.php
+++ src/Service/UrlParser.php
@@ -23,8 +23,9 @@ class UrlParser
     {
         $type = null;
         $vid = null;
+        $url = parse_url($url);
 
-        if (!$url = parse_url($url)) {
+        if (!$url || !isset($url['host'])) {
             return [$type, $vid];
         }
 
@@ -35,16 +36,16 @@ class UrlParser
         if (in_array($url['host'], self::DOMAINS[VideoEmbedder::WM_EMBED_TYPE_YOUTUBE], true)) {
             $type = VideoEmbedder::WM_EMBED_TYPE_YOUTUBE;
 
-            if ($url['host'] === 'youtu.be') {
+            if (isset($url['path']) && $url['host'] === 'youtu.be') {
                 $vid = trim($url['path'], '/');
-            } elseif (strpos($url['path'], '/embed') !== false) {
+            } elseif (isset($url['path']) && strpos($url['path'], '/embed') !== false) {
                 $vid = str_replace('/embed/', '', $url['path']);
             } elseif (isset($url['query']['v'])) {
                 $vid = $url['query']['v'];
             }
         }
 
-        if (in_array($url['host'], self::DOMAINS[VideoEmbedder::WM_EMBED_TYPE_VIMEO], true)) {
+        if (isset($url['path']) && in_array($url['host'], self::DOMAINS[VideoEmbedder::WM_EMBED_TYPE_VIMEO], true)) {
             $type = VideoEmbedder::WM_EMBED_TYPE_VIMEO;
             $vid = trim($url['path'], '/');
         }
